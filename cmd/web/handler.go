2834844966927b6ccd56e65f80ac973d953a9bbd
Venkad000 <20pt38@psgtech.ac.in>
2022-01-07 18:37:15 +0530

    first commit

---
+++ cmd/web/handler.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"log"
+	"html/template"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
+	files := []string {
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	
+	ts,err := template.ParseFiles(files...)
+	if err!=nil {
+		log.Println(err.Error())
+		http.Error(w,"Internal Server Error",508)
+		return
+	}
+
+	err = ts.Execute(w,nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w,"Internal Server Error",500)
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r*http.Request) {
+	id,err := strconv.Atoi(r.URL.Query().Get(("id")))
+	if err != nil || id <1 {
+		http.NotFound(w,r)
+		return
+	}
+	
+	fmt.Fprintf(w,"Display a specific snippet with ID %d ...",id)
+}
+
+func createSnippet(w http.ResponseWriter, r*http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow","POST")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Methods not allowed"))
+		http.Error(w,"Method not allowd",405)
+		return
+	}
+
+	w.Write([]byte("create a new snippet"))
+}
